d70ffc07481499b5d1d07ea7f8a132f9387645dc
ineke.methner <ineke.methner@10316.local>
2020-12-15 17:37:52 +0100

    color palette change

--- client/src/components/ProfileList.js
+++ client/src/components/ProfileList.js
@@ -63,7 +63,7 @@ export default class ProfileList extends Component {
                 <p><b>Speaking: </b>{user.languagesSpoken.join(', ')}</p>
                 <p><b>Learning: </b>{user.languagesLearn.join(', ')}</p>
               </Card.Text>
-              <Link to={`/${user._id}`} className='button' style={{ color: '#fb8500' }} variant="primary">Find out more</Link>
+              <Link to={`/${user._id}`} className='button' style={{ color: '#5465ff' }} variant="primary">Find out more</Link>
             </Card.Body>
           </Card>
         </div>
