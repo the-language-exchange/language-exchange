0810a5381a64d699e152b3316ab4f6f282092bb1
ineke.methner <ineke.methner@10316.local>
2020-12-16 15:38:53 +0100

    changed app.js for deployment

--- app.js
+++ app.js
@@ -76,7 +76,8 @@ app.use(require('node-sass-middleware')({
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
@@ -94,5 +95,11 @@ app.use('/api/users', users);
 
 const messages = require('./routes/messages')
 app.use('/api', messages);
+
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 module.exports = app;
